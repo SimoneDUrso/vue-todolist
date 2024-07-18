d1dc1ebc2aa1357431b0b47f5a565695994ef161
SimoneDUrso <simone.durso98@gmail.com>
2024-07-18 15:45:01 +0200

    [UPD] Created array of objects

--- js/script.js
+++ js/script.js
@@ -3,7 +3,33 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            todo: [
+                {
+                    text: "hyperX",
+                    done: false
+                },
+                {
+                    text: "Logitech",
+                    done: false
+                },
+                {
+                    text: "Asus ROG",
+                    done: false
+                },
+                {
+                    text: "Razer",
+                    done: false
+                },
+                {
+                    text: "Trust",
+                    done: false
+                },
 
+            ]
         }
+    },
+
+    methods: {
+
     }
 }).mount("#app")
