4baec7b3d7b719b30fb77d96cf0a45ae7e250277
SimoneDUrso <simone.durso98@gmail.com>
2024-07-18 17:01:29 +0200

    [UPD] Added bonus folder

---
+++ bonus/js/script.js
@@ -0,0 +1,67 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            todos: [
+                {
+                    text: "Faccio colazione",
+                    done: false
+                },
+                {
+                    text: "Seguo la lezione di Fabrizio",
+                    done: false
+                },
+                {
+                    text: "Cucino il pranzo",
+                    done: false
+                },
+                {
+                    text: "Faccio gli esercizi per Boolean",
+                    done: false
+                },
+                {
+                    text: "Faccio merenda",
+                    done: false
+                },
+                {
+                    text: "Gioco ad Elden Ring",
+                    done: false
+                },
+                {
+                    text: "Guardo un film",
+                    done: false
+                },
+                {
+                    text: "Vado a letto",
+                    done: false
+                },
+
+            ],
+
+            newTodo: null
+        }
+    },
+
+    methods: {
+        todoBarred(index) {
+            this.todos[index].done = true;
+            console.log(this.todos[index].done)
+        },
+
+        deleteTodo(index) {
+            if (confirm("Sei sicuro di voler eliminare questa Todo?")) {
+                this.todos.splice(index, 1);
+            }
+        },
+
+        addTodo() {
+            let emptyObject = {
+                text: this.newTodo,
+                done: false
+            }
+            this.todos.push(emptyObject)
+            this.newTodo = null
+        }
+    }
+}).mount("#app")
